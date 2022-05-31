0ea9f90d9b7292855c961b8ef602825c6828045a
Victoria Parry <vlparry@gmail.com>
2022-05-31 12:51:38 -0400

    commit tree 95b97739ef2344601330433f829f4c87a430852a

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true
+    }
+  }
+  return false
 }
+
+// function hasTargetSum(array, target){
+//   const seenNumbers = {};
+  
+//   for (const number of array){
+//     if (complement in seenNumbers) return true;
+//     seenNumbers[number] = true;
+//   }
+  // second soluton:::
+//   // (let i = 0; i < array.length; i++){
+//   //   const complement = target - array [i];
+//   //   if (seenNumbers[complement]) return true
+//   //   seenNumbers[array[i]] = true
+//   // }
+//   return false
+// }
 
 /* 
   Write the Big O time complexity of your function here
+runtime: 0(n)
+
+
+  runtime: O(n^2)
+  space: O(n)
 */
 
 /* 
   Add your pseudocode here
-*/
+create an object to keep track of numbers weve already seen.
+
+iterate through each number in the array
+for the current number, identify a complement that adds to the target(comp = target - num)
+
+check if any key on our object is the complement
+if so, return true
+otherwise, add that number to the object
+
+
+
+
+
+iterate throiugh the rest of the array and check if any number is our complement.
+if so, return true.
+
+if I reach the end of the array, return false.
+
+
+
+~~~
+  make a function called hasTargetSum that checks if 2 numbers from the array add up to some target. For example, if I have an array [1,2,3,4] and the target is 6 I should return true because 2 and 4 add up to 6. For each number, check if there is another number that adds to the target. 
 
 /*
   Add written explanation of your solution here
@@ -29,6 +77,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
